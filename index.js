44dc83999884a303604e13966d65d9408f04d338
andre <andremorais0000@gmail.com>
2024-07-15 23:22:48 -0300

    Added estudo do mecanismo de busca Name's document.getElement

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 
     getElementById -> Trás UM elemento pelo ID
 
+    getElementByClassName -> Trás TODOS os elementos com essa CLASSE
+    getElementByTagName -> Trás TODOS os elementos com essa TAG
+    getElementByName -> Trás TODOS os elementos com esse NOME
 
 */
 
 
 
 document.getElementById("Coloco Aqui O Id Descrito No HTML")
+
+document.getElementsByClassName("Coloco Aqui A Classe Descrita No HTML")
+
+document.getElementsByTagName("Coloco Aqui A TAG Descrita No HTML")
+
+document.getElementsByName("Coloco Aqui O Nome Do Campo Que Quero") // atributo name="nome-do-atributo"
