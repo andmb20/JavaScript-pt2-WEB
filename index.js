9e8bc7d36b11d455f00909911634fd2718be0f70
andre <andremorais0000@gmail.com>
2024-07-15 23:37:16 -0300

    Added estudo do mecanismo de busca querySelector e querySelectorAll

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
     getElementByTagName -> Trás TODOS os elementos com essa TAG
     getElementByName -> Trás TODOS os elementos com esse NOME
 
+    querySelector -> Trás UM elemento, o PRIMEIRO que ele encontrar
+    querySelectorAll -> Trás TODOS os elementos que encontrar
+
 */
 
 
@@ -18,3 +21,11 @@ document.getElementsByClassName("Coloco Aqui A Classe Descrita No HTML")
 document.getElementsByTagName("Coloco Aqui A TAG Descrita No HTML")
 
 document.getElementsByName("Coloco Aqui O Nome Do Campo Que Quero") // atributo name="nome-do-atributo"
+
+document.querySelector("Podemos Utilizar TAG")
+document.querySelector(".Podemos Utilizar CLASSE") // com "." -> estou dizendo para procurar uma CLASSE
+document.querySelector("#Podemos Utilizar ID") // com "#" -> estou dizendo para procurar um ID
+document.querySelector("Podemos Utilizar TAG.Utilizar CLASSE") // para quando tivermos a mesma CLASSE em mais de uma TAG / Serve como um "filtro" para trazer exclusivamente o que queremos
+
+document.querySelectorAll("TAG") // trás todos com a mesma tag
+document.querySelectorAll(".Nome Da Classe") // trás todos com a mesma classe
